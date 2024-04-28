090a6ea364cf3833742d84d4477cb3430b6cdbbc
syakilaspace <putrisyakila2017@gmail.com>
2024-04-28 17:08:40 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi eveniet nesciunt eos labore non tempora sit ex quisquam nemo nam placeat beatae, aliquid quis ullam tenetur ad cumque illo rerum!
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
 </body>
 </html>
